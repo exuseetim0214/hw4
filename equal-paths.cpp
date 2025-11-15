84aa8549bfbca6b0acecda3bf88e332299a5a681
exuseetim0214 <timzhou@usc.edu>
2025-11-15 00:18:52 -0800

    Add height function to validate equal paths in tree
    Implement height function to check if all paths in a binary tree have the same length.

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -9,10 +9,41 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int height(Node* root)
+{
+    if(root == NULL){
+        return 1;
+    }
 
+    //leaf
+    if(root->left == NULL && root ->right == NULL){
+        return 1;
+    }
+
+    int left_height = height(root -> left);
+    int right_height = height(root -> right);
+
+    if(left_height == -1 || right_height == -1){
+        return -1;
+    }
+
+    if(root->left != NULL && root->right != NULL){
+        if(left_height != right_height){
+            return -1;
+        }
+        return left_height + 1;
+    }
+    if(root-> left != NULL){
+        return left_height +1;
+    }
+    else{
+        return right_height +1;
+    }
+
+}
 bool equalPaths(Node * root)
 {
     // Add your code below
-
+    return height(root) != -1;
 }
 
